0fe22df95e8000c905a0f3b4cd9317b1e5e46461
Prajwal Hegde <prajwal@params-mbp.lan>
2020-03-18 17:54:37 -0400

    Added

---
+++ react-client/src/app/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { NavBar } from '../components'
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+        </Router>
+    )
+}
+
+export default App
